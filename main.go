7e9f5bca5b6a625ffc63d86971dd38608ebbc429
Sandra Juarez <sandjuarezg@gmail.com>
2022-03-13 14:43:13 -0600

    refactor: decryption exemplification

--- main.go
+++ main.go
@@ -2,63 +2,24 @@ package main
 
 import (
 	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"fmt"
-	"io"
 	"log"
 )
 
 func main() {
-	text := []byte("This message is secret wiuuuuuuuuuuui top secret")
-	key := []byte("123456789abcdefghijklmnopqrstuvw")
+	key := []byte("123456789abcdefghijklmno")
+	text := []byte("This message is secret wiu top secret")
 
-	// encrypt
-	// create a block cipher
-	cpbl, err := aes.NewCipher(key)
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// cipher package - implements standard block cipher
-	//
-	gcm, err := cipher.NewGCM(cpbl)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-
-	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	encrypt := gcm.Seal(nonce, nonce, text, nil)
-
+	encrypt := make([]byte, block.BlockSize())
+	block.Encrypt(encrypt, text)
 	fmt.Println(string(encrypt))
 
-	// decrypt
-	c, err := aes.NewCipher(key)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	gcm, err = cipher.NewGCM(c)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	nonceSize := gcm.NonceSize()
-	if len(encrypt) < nonceSize {
-		fmt.Println(err)
-	}
-
-	nonce, ciphertext := encrypt[:nonceSize], encrypt[nonceSize:]
-	decrypt, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-
+	decrypt := make([]byte, 1024)
+	block.Decrypt(decrypt, encrypt)
 	fmt.Println(string(decrypt))
 }
